71f50346ce6fd7b617cf3741ad1b54ab3fdf3d0a
bigwiggerz <bigwiggeroffical@gmail.com>
2024-05-02 16:07:01 +0300

    feat: finished delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,21 @@
+import { existsSync } from 'fs';
+import { unlink } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
+const filePath = join(__dirname, 'files', 'fileToRemove.txt');
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(filePath)) throw new Error('FS operation failed');
+
+    await unlink(filePath);
+  } catch (e) {
+    console.error(e.message);
+  }
 };
 
-await remove();
+
+remove();
