18da59261e8072d156150a83f0d62d2618b61c9e
bigwiggerz <bigwiggeroffical@gmail.com>
2024-05-02 16:34:41 +0300

    feat: finished calchash.js

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,16 @@
+import { createHash } from 'crypto';
+import { readFile } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+const filePath = join(__dirname, 'files', 'fileToCalculateHashFor.txt');
+
 const calculateHash = async () => {
-    // Write your code here 
+  const fileContent = await readFile(filePath);
+  const hash = createHash('sha256').update(fileContent).digest('hex');
+
+  console.log('SHA256 hash of %s is: %s', filePath, hash);
 };
 
 await calculateHash();
--- src/modules/cjsToEsm.cjs
+++ src/modules/cjsToEsm.cjs
@@ -1,27 +1,28 @@
-const path = require('path');
-const { release, version } = require('os');
-const { createServer: createServerHttp } = require('http');
-require('./files/c');
+import * as path from 'path'
+import { release, version } from 'os'
+import { createServer as createServerHttp } from 'http';
+import { readFile } from 'fs/promises';
+await import('./files/c.js')
 
 const random = Math.random();
 
 let unknownObject;
 
 if (random > 0.5) {
-    unknownObject = require('./files/a.json');
+  unknownObject = JSON.parse(await readFile(path.join(import.meta.dirname, 'files', 'a.json')))
 } else {
-    unknownObject = require('./files/b.json');
+  unknownObject = JSON.parse(await readFile(path.join(import.meta.dirname, 'files', 'b.json')))
 }
 
 console.log(`Release ${release()}`);
 console.log(`Version ${version()}`);
 console.log(`Path segment separator is "${path.sep}"`);
 
-console.log(`Path to current file is ${__filename}`);
-console.log(`Path to current directory is ${__dirname}`);
+console.log(`Path to current file is ${import.meta.filename}`);
+console.log(`Path to current directory is ${import.meta.dirname}`);
 
 const myServer = createServerHttp((_, res) => {
-    res.end('Request accepted');
+  res.end('Request accepted');
 });
 
 const PORT = 3000;
@@ -29,12 +30,11 @@ const PORT = 3000;
 console.log(unknownObject);
 
 myServer.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
-    console.log('To terminate it, use Ctrl+C combination');
+  console.log(`Server is listening on port ${PORT}`);
+  console.log('To terminate it, use Ctrl+C combination');
 });
 
-module.exports = {
-    unknownObject,
-    myServer,
-};
-
+export default {
+  unknownObject,
+  myServer
+}
