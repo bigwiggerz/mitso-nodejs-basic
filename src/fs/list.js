4f50a541cf0aaac93847eacd1a52290f5d1dbd3e
bigwiggerz <bigwiggeroffical@gmail.com>
2024-05-02 16:12:23 +0300

    feat: finished list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,22 @@
+import { existsSync } from 'fs';
+import { readdir } from 'fs/promises';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const dirPath = join(__dirname, 'files');
+
 const list = async () => {
-    // Write your code here 
+  try {
+    if (!existsSync(dirPath)) throw new Error('FS operation failed');
+
+    const files = await readdir(dirPath);
+    console.log(files);
+  } catch (e) {
+    console.error(e.message);
+  }
 };
 
 await list();
