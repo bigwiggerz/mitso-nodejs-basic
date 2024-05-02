a9b46385e4b77d45be2987347ec573f42a1e3aca
bigwiggerz <bigwiggeroffical@gmail.com>
2024-05-02 16:20:33 +0300

    feat: finished env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,18 @@
 const parseEnv = () => {
-    // Write your code here 
+    
+
+    const prefix = 'MITSO_';
+    const envVariables = process.env;
+    const mitsoVariables = Object.keys(envVariables)
+        .filter(key => key.startsWith(prefix))
+        .map(key => `${key}=${envVariables[key]}`)
+        .join('; ');
+    if (mitsoVariables) {
+        console.log(mitsoVariables);
+    } else {
+        console.error('No MITSO variables found in the environment.');
+    }
+
 };
 
 parseEnv();
