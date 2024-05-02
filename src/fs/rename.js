8faa1a580ed7a71cb5a5258b9391f26c8e23dd3b
bigwiggerz <bigwiggeroffical@gmail.com>
2024-05-02 16:01:19 +0300

    feat: finished rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,22 @@
+import { existsSync } from 'fs';
+import { rename as fsRename } from 'fs/promises';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url));
+const srcFile = join(__dirname, 'files', 'wrongFilename.txt');
+const newFilename = 'properFilename.md';
+const newFilenamePath = join(__dirname, 'files', newFilename);
+
 const rename = async () => {
-    // Write your code here 
+  try {
+    if (existsSync(newFilenamePath) || !existsSync(srcFile))
+      throw new Error('FS operation failed');
+
+    await fsRename(srcFile, newFilenamePath);
+  } catch (e) {
+    console.error(e.message);
+  }
 };
 
 await rename();
