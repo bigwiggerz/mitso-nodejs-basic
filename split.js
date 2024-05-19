f415d6241a2e2975943044a29cc62ba7e557a7ea
bigwiggerz <bigwiggeroffical@gmail.com>
2024-05-20 02:18:27 +0300

    feat: realize split.js add cli

--- split.js
+++ split.js
@@ -1,4 +1,5 @@
 import { program } from 'commander';
+import { readFileSync, writeFileSync } from 'fs';
 
 function splitNumbers(input) {
     const numbers = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
@@ -13,10 +14,36 @@ function splitNumbers(input) {
 }
 
 program
-  .option('-n, --numbers <type>', 'input string of numbers')
+  .requiredOption('-t, --task <type>', 'task to execute')
+  .option('-i, --input <type>', 'input file')
+  .option('-o, --output <type>', 'output file')
   .action((options) => {
-    const result = splitNumbers(options.numbers);
-    console.log(result);
+    let input;
+    if (options.input) {
+      // Если входной файл указан, читаем данные из файла
+      input = readFileSync(options.input, 'utf8');
+    } else {
+      console.error('Input file is required');
+      process.exit(1);
+    }
+
+    let result;
+    switch (options.task) {
+      case 'splitNumbers':
+        result = splitNumbers(input);
+        break;
+      default:
+        console.error(`Unknown task: ${options.task}`);
+        process.exit(1);
+    }
+
+    if (options.output) {
+      // Если выходной файл указан, записываем результат в файл
+      writeFileSync(options.output, result);
+    } else {
+      // Или выводим результат в stdout
+      console.log(result);
+    }
   });
 
 program.parse(process.argv);
